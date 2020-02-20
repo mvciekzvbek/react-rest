43975468e34903903e67d86cb7ab549e1a90aa02
mvciekzvbek <maciej.jan.zabek@gmail.com>
2020-02-20 22:32:21 +0100

    bearer

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ ReactDOM.render(
     <Auth0Provider
       domain={process.env.REACT_APP_AUTH0_DOMAIN}
       client_id={process.env.REACT_APP_AUTH0_CLIENT_ID}
-      // audience={process.env.REACT_APP_AUTH0_AUDIENCE}
+      audience={process.env.REACT_APP_AUTH0_AUDIENCE}
       redirect_uri={window.location.origin}
       onRedirectCallback={onRedirectCallback}
     >
